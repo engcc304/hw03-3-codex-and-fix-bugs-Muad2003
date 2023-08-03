b3195c432d58c3eaaa757e19b8fb815f5c82c1c3
Muad2003 <anuphap2003118@gmail.com>
2023-08-03 17:22:25 +0700

    update homework 3-3

--- Homework/HW03/hw03-3-codex-and-fix-bug.cpp
+++ Homework/HW03/hw03-3-codex-and-fix-bug.cpp
@@ -5,27 +5,27 @@ int main() {
     int rows1, cols1, rows2, cols2, rows3, cols3, rows4, cols4;
     int i, j, k;
 
-    printf("Enter Matrix1:\n");
-    printf("Enter number of rows and columns: ");
+    printf("Enter Matrix1 : \n");
+    printf("Enter number of rows and columns : ");
     scanf("%d %d", &rows1, &cols1);
     for (i = 0; i < rows1; i++) {
         for (j = 0; j < cols1; j++) {
-            printf("Enter [%d %d] value: ", i, j);
+            printf("Enter [%d %d] value : ", i, j);
             scanf("%d", &Matrix1[i][j]);
         }
     }
 
-    printf("\nEnter Matrix2:\n");
-    printf("Enter number of rows and columns: ");
+    printf("\nEnter Matrix2 : \n");
+    printf("Enter number of rows and columns : ");
     scanf("%d %d", &rows2, &cols2);
     for (i = 0; i < rows2; i++) {
         for (j = 0; j < cols2; j++) {
-            printf("Enter [%d %d] value: ", i, j);
+            printf("Enter [%d %d] value : ", i, j);
             scanf("%d", &Matrix2[i][j]);
         }
     }
 
-    printf("\nMatrix1:\n");
+    printf("\nMatrix1 : \n");
     for (i = 0; i < rows1; i++) {
         for (j = 0; j < cols1; j++) {
             printf("%d ", Matrix1[i][j]);
@@ -33,7 +33,7 @@ int main() {
         printf("\n");
     }
 
-    printf("\nMatrix2:\n");
+    printf("\nMatrix2 : \n");
     for (i = 0; i < rows2; i++) {
         for (j = 0; j < cols2; j++) {
             printf("%d ", Matrix2[i][j]);
@@ -49,7 +49,7 @@ int main() {
         }
     }
 
-    printf("\nTranspose of Matrix1 is:\n");
+    printf("\nTranspose of Matrix1 is : \n");
     for (i = 0; i < rows3; i++) {
         for (j = 0; j < cols3; j++) {
             printf("%d ", Matrix3[i][j]);
@@ -70,14 +70,13 @@ int main() {
                 }
             }
         }
-        printf("\nMatrix1 x Matrix2 is:\n");
+        printf("\nMatrix1 x Matrix2 is : \n");
         for (i = 0; i < rows4; i++) {
             for (j = 0; j < cols4; j++) {
                 printf("%d ", Matrix4[i][j]);
             }
             printf("\n");
-        }
-    }
-
+        } //end for
+    } //end else
     return 0;
 }
